aeaab986321c4e52fdc74a9bdbdc1a799cdbab48
Joel Schaerer <joelthelion@laposte.net>
2010-11-12 18:49:25 +0100

    now we need to connect the backend to the frontend

--- vote.py
+++ vote.py
@@ -10,22 +10,34 @@ from prenom import Ui_MainWindow
 class Ballots:
     def __init__(self,filename):
         """open file and parse ballots"""
-        #ballots[frozenset(p1,p2)]=(sep,count)
-        ballots={}
+        #ballots[frozenset(p1,p2)]=(winner,sep,other,count)
+        self.ballots={}
+        self.filename=filename
+        try:
+            for l in open(filename):
+                l.strip()
+                self.add(self.parse_ballot(l))
+        except IOError:
+            print "Ballot file not found"
     def is_in(self,ballot):
         return self.get_couple(ballot) in self.ballots.keys()
     def get_couple(self,ballot):
         winner,sep,other,count=ballot
+        winner,sep,other,count=ballot
+        winner=winner.capitalize()
+        other=other.capitalize()
         couple=frozenset((winner,other))
         return couple
     def add(self,ballot):
         winner,sep,other,count=ballot
+        winner=winner.capitalize()
+        other=other.capitalize()
         if not self.is_in(ballot):
-            self.ballots[self.get_couple(ballot)]=(sep,count)
+            self.ballots[self.get_couple(ballot)]=(winner,sep,other,count)
         else:
             old_sep,old_count=self.ballots[self.get_couple(ballot)]
             assert(old_sep==sep)
-            self.ballots[self.get_couple(ballot)]=(sep,old_count+count)
+            self.ballots[self.get_couple(ballot)]=(winner,sep,other,old_count+count)
     def parse_ballot(self,string):
         count,rest=string.split(":")
         count=int(count)
@@ -33,6 +45,13 @@ class Ballots:
         #We don't support multiple ballots
         assert(not re.search("([^=^>]*)(=|>)(.*)",other))
         return (winner,sep,other,count)
+    def ballot_repr(self,ballot):
+        winner,sep,other,count=ballot
+        return repr(count)+":"+winner+sep+loser
+    def save(self):
+        with open(self.filename,"w") as f:
+            for ballot in self.ballots.values():
+                f.write(self.ballot_repr(ballot)+"\n")
 
 class StartQT4(QtGui.QMainWindow):
     def __init__(self, parent=None):
@@ -43,6 +62,14 @@ class StartQT4(QtGui.QMainWindow):
 
 if __name__ == "__main__":
     prenoms=[x.strip() for x in open("prenoms.txt")]
+    b=Ballots("ballots.txt")
+    b.save()
+    combis=[]
+    for n,p in enumerate(prenoms):
+        for p2 in prenoms[n+1:]:
+            combis.append((p,p2))
+    import random
+    random.shuffle(combis)
     #print prenoms
     app = QtGui.QApplication(sys.argv)
     myapp = StartQT4()
