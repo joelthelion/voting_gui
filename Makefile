c5f837f2ee999f41f2f55d8bbbb786b7937d6115
Joel Schaerer <joelthelion@laposte.net>
2010-11-12 16:04:26 +0100

    some progress

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
+all: prenom.py resource_rc.py
+
 prenom.py: prenom.ui
 	pyuic4 prenom.ui > prenom.py
+resource_rc.py: resource.qrc nounours_cropped.png
+	pyrcc4 resource.qrc > resource_rc.py
---
+++ vote.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import sys
+from PyQt4 import QtCore, QtGui
+from prenom import Ui_MainWindow
+
+class StartQT4(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = StartQT4()
+    myapp.show()
+    sys.exit(app.exec_())
+
